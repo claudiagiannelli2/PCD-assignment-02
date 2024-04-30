ef9cf39630379eed38f920a22e9cfea557af6da4
Eugenio Tampieri <eugenio@eutampieri.eu>
2024-04-30 14:21:52 +0200

    Added build directory in gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
     implementation("io.vertx:vertx-web:4.5.7")
     implementation("io.vertx:vertx-web-client:4.5.7")
     implementation("io.reactivex.rxjava3:rxjava:3.1.8")
-       
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -39,7 +38,7 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.App"
+    mainClass = "pcd.ass02.rx.Ass02rxGUI"
 }
 
 tasks.named<Test>("test") {
--- app/src/main/java/pcd/ass02/WebPageExtractor.java
+++ app/src/main/java/pcd/ass02/WebPageExtractor.java
@@ -2,20 +2,28 @@ package pcd.ass02;
 
 import java.net.*;
 import java.io.*;
+import com.gitlab.pdftk_java.com.lowagie.text.pdf.PdfReader;
 
 public final class WebPageExtractor extends ExtractionTask<URL, Webpage> {
     public Webpage extract(URL u) {
         try {
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(u.openStream()));
+            String contentType = u.openConnection().getHeaderField("Content-Type");
+            if(contentType.equals("application/pdf")) {
+                PdfReader pdf = new PdfReader(u.openStream());
 
-            String body = "";
-            String inputLine;
-            while ((inputLine = in.readLine()) != null){
-                body = body + inputLine + "\n";
+                return new Webpage("", u);
+            } else {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(u.openStream()));
+
+                String body = "";
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    body = body + inputLine + "\n";
+                }
+                in.close();
+                return new Webpage(body, u);
             }
-            in.close();
-            return new Webpage(body, u);
         } catch (Exception e) {
             return new Webpage("", u);
         }
